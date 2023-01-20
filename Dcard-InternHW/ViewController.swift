e06989832f139567a3eac8d1e4ee02e4af553447
Michael Namara <michaelnamara0326@gmail.com>
2023-01-20 20:24:50 +0800

    Initial Commit

---
+++ Dcard-InternHW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dcard-InternHW
+//
+//  Created by Michael Namara on 2023/1/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
