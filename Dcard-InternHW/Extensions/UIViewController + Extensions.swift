065c5dd2bd5f8b74999b71787d49fbf39636fa1a
Michael Namara <michaelnamara0326@gmail.com>
2023-01-26 18:13:39 +0800

    fix: update setting

--- Dcard-InternHW/Extensions/UIViewController + Extensions.swift
+++ Dcard-InternHW/Extensions/UIViewController + Extensions.swift
@@ -8,13 +8,5 @@
 import UIKit
 
 extension UIViewController {
-    func hideKeyboardWhenTappedAround() {
-        let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
-        tap.cancelsTouchesInView = false
-        view.addGestureRecognizer(tap)
-    }
     
-    @objc func dismissKeyboard() {
-        view.endEditing(true)
-    }
 }
